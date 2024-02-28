5b452a04e9989865adbf2e63506d548bf04d95a5
Vitalij Kazuro <vitalijkazuro939@gmail.com>
2024-02-28 15:21:43 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 "use strict";
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 2,
+  centeredSlides: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   const modal = document.getElementById("modal");
   const closeBtnModal = document.querySelector(".modal__close");
